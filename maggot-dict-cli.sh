568176dfad1018ef3c693ee1fc7bd8211a485fc5
aslpavel <asl.pavel@gmail.com>
2012-10-17 18:33:42 +0400

    [compltion] changed: install in /etc/profile.d

---
+++ maggot-dict-cli.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+# initialize maggot-dict-cli
+
+alias d='maggot-dict-cli'
+
+complete -C maggot-dict-cli maggot-dict-cli
+complete -C maggot-dict-cli d
+
+# vim: nu ft=sh columns=120 :
